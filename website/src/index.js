91080b16bb7684dc9a4ef9983acd0aca3c5f7030
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-11-10 23:46:49 +1100

    displaying dummy messages

--- website/src/index.js
+++ website/src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+       messages: DUMMY_DATA
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <MessageList messages={this.state.messages}/>
+        {/* <SendMessageForm /> */}
+     </div>
+    )
+  }
+}
+
+class MessageList extends React.Component {
+
+  render() {
+    return (
+      <ul className="message-list">                 
+        {this.props.messages.map((message, index) => {
+          return (
+           <li key={index}>
+             <div>
+               <b>{message.senderId}</b>
+             </div>
+             <div>
+               {message.text}
+             </div>
+           </li>
+         )
+       })}
+     </ul>
+    )
+  }
+}
+
+function Title() {
+  return <p className="title">My Dental Clinic chat bot</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
