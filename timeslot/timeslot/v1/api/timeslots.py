76379bb67c8d54223bd81d5847327a1ddedab97a
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-11-17 00:59:47 +1100

    returning valid timeslots

--- timeslot/timeslot/v1/api/timeslots.py
+++ timeslot/timeslot/v1/api/timeslots.py
@@ -5,10 +5,19 @@ from flask import request, g
 
 from . import Resource
 from .. import schemas
+import json
 
 
 class Timeslots(Resource):
 
     def get(self):
+        timeslots = read_from_file()
+        data = timeslots['timeslots']
+        resp = {'data': data}
+        return resp, 200, None
 
-        return {}, 200, None
+
+def read_from_file():
+    fname = 'valid_timeslots.json'
+    with open(fname, "r") as books:
+        return json.loads(books.read())
