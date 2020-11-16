16c9ccf090d38539e1273f776fde5c950d9dcfcb
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-11-17 02:05:59 +1100

    implemented get bookings or get booking by dentistName, time and/or patientName

--- timeslot/timeslot/v1/api/bookings.py
+++ timeslot/timeslot/v1/api/bookings.py
@@ -9,13 +9,25 @@ import hashlib
 from timeslot.timeslot.v1.Booking import Booking
 import json
 
+
 class Bookings(Resource):
 
     def get(self):
-        print(g.args)
-        
+        arguments = g.args
+        dentistName, time, patientName = fetch_params_from_arg(arguments)
+        all_bookings = Booking.read_from_file()
+        all_bookings = all_bookings['bookings']
 
-        return {}, 200, None
+        if dentistName:
+            all_bookings = get_by_dentistName(all_bookings, dentistName)
+        if time:
+            time = int(time)
+            all_bookings = get_by_time(all_bookings, time)
+        if patientName:
+            all_bookings = get_by_patientName(all_bookings, patientName)
+
+        resp = {'data': all_bookings}
+        return resp, 200, None
 
     def post(self):
         dentistName = str(request.json['dentistName'])
@@ -31,11 +43,41 @@ class Bookings(Resource):
         resp = {'id': id}
         return resp, 201, None
 
+
+def fetch_params_from_arg(arguments):
+    dentistName = arguments.get('dentistName')
+    time = arguments.get('dentistName')
+    patientName = arguments.get('patientName')
+
+    return dentistName, time, patientName
+
+
+def get_by_dentistName(all_bookings: list, dentistName: str):
+    vals = list()
+    for booking in all_bookings:
+        if booking['dentistName'] == dentistName:
+            vals.append(booking)
+    return vals
+
+
+def get_by_time(all_bookings: list, time: int):
+    vals = list()
+    for booking in all_bookings:
+        if booking['time'] == time:
+            vals.append(booking)
+    return vals
+
+
+def get_by_patientName(all_bookings: list, patientName: str):
+    vals = list()
+    for booking in all_bookings:
+        if booking['patientName'] == patientName:
+            vals.append(vals)
+    return vals
+
+
 def generate_hash_id(dentistName: str, time: int, patientName: str):
     s = dentistName + str(time) + patientName
-    return hashlib.md5(s.encode('utf-8')).hexdigest()[1:6]
-
-def read_from_file():
-    fname = 'bookings.json'
-    with open(fname, "r") as books:
-        return json.loads(books.read())
+    s = hashlib.md5(s.encode('utf-8')).hexdigest()
+    s = s[:5]
+    return s
