c2717b93ec79edb8bf22bc06550a23c3b62a7cdb
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-11-17 03:11:17 +1100

    removed redundant parenthesis

--- chatbot/chatbot/v1/api/Patient.py
+++ chatbot/chatbot/v1/api/Patient.py
@@ -1,6 +1,5 @@
-class Patient():
+class Patient:
     def __init__(self, name):
-        self.name = None
         self.dentistName = None
         self.time = None
         self.getAllDentists = False
